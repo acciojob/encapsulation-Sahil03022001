3ad99a34b9fc8c5985a99d1ec27c874e2793f988
Sahil Dhiman <dhiman.sahil03022001@gmail.com>
2023-05-09 22:47:03 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+
+        //java: name has private access in com.driver.RWOnly
+//        rwOnly.name = "SAHIL";
+//        System.out.println(rwOnly.name);
+
+        rwOnly.setName("SAHIL");
+        System.out.println(rwOnly.getName());
+    }
 }
